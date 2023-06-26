976fed7bfe1b0d631b593121d41266d75704124b
yuhuakukude <yuhuakukude@gmail.com>
2023-06-27 00:09:43 +0800

    setup chain id

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'B2',
-      'B2swap'
+      'Shdw-LP',
+      'Shadow LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
