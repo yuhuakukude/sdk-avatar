9953a0741938b178999981b183009d57b2740693
yuhuakukude <yuhuakukude@gmail.com>
2023-06-26 23:13:43 +0800

    setup chain id

--- dist/sdk.cjs.development.js
+++ dist/sdk.cjs.development.js
@@ -34,8 +34,8 @@ var _SOLIDITY_TYPE_MAXIMA;
   Rounding[Rounding["ROUND_UP"] = 2] = "ROUND_UP";
 })(exports.Rounding || (exports.Rounding = {}));
 
-var FACTORY_ADDRESS = '0x326Ee96748E7DcC04BE1Ef8f4E4F6bdd54048932';
-var INIT_CODE_HASH = '0xcfeb1f8754759f0abe461dd409d6d501fdd73b91a848c483faa6db0faf53c3bd';
+var FACTORY_ADDRESS = '0xdf5340e76d2bCe89668F633D095AB21587E3128D';
+var INIT_CODE_HASH = '0x92ef6da22857152070f5a36775fdd63bb261283443d86ae06cb4bb0756ed7de8';
 var MINIMUM_LIQUIDITY = /*#__PURE__*/JSBI.BigInt(1000); // exports for internal consumption
 
 var ZERO = /*#__PURE__*/JSBI.BigInt(0);
