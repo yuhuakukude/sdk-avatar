2344f819026f8fe6170d078e52fab0b8b4b59e2f
yuhuakukude <yuhuakukude@gmail.com>
2023-06-27 00:42:04 +0800

    1.0.10

--- dist/sdk.cjs.development.js
+++ dist/sdk.cjs.development.js
@@ -35,7 +35,7 @@ var _SOLIDITY_TYPE_MAXIMA;
 })(exports.Rounding || (exports.Rounding = {}));
 
 var FACTORY_ADDRESS = '0x326Ee96748E7DcC04BE1Ef8f4E4F6bdd54048932';
-var INIT_CODE_HASH = '0xcfeb1f8754759f0abe461dd409d6d501fdd73b91a848c483faa6db0faf53c3bdbytes32';
+var INIT_CODE_HASH = '0xcfeb1f8754759f0abe461dd409d6d501fdd73b91a848c483faa6db0faf53c3bd';
 var MINIMUM_LIQUIDITY = /*#__PURE__*/JSBI.BigInt(1000); // exports for internal consumption
 
 var ZERO = /*#__PURE__*/JSBI.BigInt(0);
--- src/constants.ts
+++ src/constants.ts
@@ -20,7 +20,7 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS = '0x326Ee96748E7DcC04BE1Ef8f4E4F6bdd54048932'
 
-export const INIT_CODE_HASH = '0xcfeb1f8754759f0abe461dd409d6d501fdd73b91a848c483faa6db0faf53c3bdbytes32'
+export const INIT_CODE_HASH = '0xcfeb1f8754759f0abe461dd409d6d501fdd73b91a848c483faa6db0faf53c3bd'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
